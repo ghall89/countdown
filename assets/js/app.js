160f52a25d2f2ce71ff137575d6d11ebed0df606
Graham Hall <ghall89@me.com>
2021-03-13 13:41:53 -0500

    added api call

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,17 @@
 
-console.log("the countdown is on!")
-var test = function() {
-    console.log("It's working")
+const questions = function() {
+    const apiVariable = `https://opentdb.com/api.php?amount=15&category=18&difficulty=easy`
+    fetch(apiVariable).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data){
+                console.log(data);
+            })
+        } else {
+            // Change to a modal
+            alert("countDown is down");
+        }
+    });
 }
+
+questions();
 
