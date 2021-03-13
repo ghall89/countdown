91d8b2cc917b375b337c41a9b98ca9d22f06ad74
Graham Hall <ghall89@me.com>
2021-03-13 14:36:38 -0500

    check for right and wrong answers

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,15 @@
 
+let userScore = 0;
+
 const getQuestions = function() {
     const apiVariable = `https://opentdb.com/api.php?amount=15&category=18&difficulty=easy`
     fetch(apiVariable).then(function(response){
         if (response.ok) {
             response.json().then(function(data){
                 console.log(data);
-
+                let questionNumber = 0;
                 console.log(data.results[0].question);
-                showQuestions(data);
+                showQuestions(data, questionNumber);
             })
         } else {
             // Change to a modal
@@ -16,22 +18,34 @@ const getQuestions = function() {
     });
 };
 
-const showQuestions = function(data) {
-    $(".questions-container").empty().append(data.results[0].question);
+const showQuestions = function(data, questionNumber) {
+    $(".questions-container").empty().append(data.results[questionNumber].question);
 
     const answers = [];
-    answers.push(data.results[0].correct_answer);
-    for (let i = 0; i < data.results[0].incorrect_answers.length; i++) {
-        answers.push(data.results[0].incorrect_answers[i]);
+    answers.push(data.results[questionNumber].correct_answer);
+    for (let i = 0; i < data.results[questionNumber].incorrect_answers.length; i++) {
+        answers.push(data.results[questionNumber].incorrect_answers[i]);
     }
     console.log(answers);
     $(".answers-container").empty();
     for (let i = 0; i < answers.length; i++) {
         const button = $("<button/>").text(answers[i]).click(function() {
-            console.log("It's working");
+            console.log(this.innerText);
+            if (this.innerText === data.results[questionNumber].correct_answer) {
+                console.log("Correct answer");
+                userScore = userScore + 25;
+                console.log(userScore);
+                questionNumber = questionNumber + 1;
+                setTimeout(showQuestions, 3000);
+            } else {
+                console.log("WRONG!");
+                questionNumber = questionNumber + 1;
+                setTimeout(showQuestions, 3000);
+            }
         })
         $(".answers-container").append(button);
     }
+
 }
 
 getQuestions();
