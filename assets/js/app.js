34b79fc4666d4f0eece4651b6276c489cc3eb8bf
Hung Tang <hungtang@hotmail.com>
2021-03-13 13:57:47 -0800

    added write to localStorage and also get from localStorage fxns

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
-console.log("the countdown is on!")
+
+// Write to localStorage ********************************
+// Pass a full object array nameObjArry = [{initial: xyz, score: 123}, {initial: abc, score: 345}, .....]
+var writeToStorage = function(nameObjArry) {
+    localStorage.setItem("scoreList", JSON.stringify(nameObjArry));
+}
+
+// Return what's stored in localStorage *******************************
+var readFromStorage = function() {
+    let scoreList = JSON.parse(localStorage.getItem("scoreList"));
+    return scoreList; // an object array
+}
