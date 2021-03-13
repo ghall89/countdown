fe1aec5f51df5556f18039bbf0bfba63c3df16a1
Graham Hall <ghall89@me.com>
2021-03-13 14:11:09 -0500

    show question

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,38 @@
 
-const questions = function() {
+const getQuestions = function() {
     const apiVariable = `https://opentdb.com/api.php?amount=15&category=18&difficulty=easy`
     fetch(apiVariable).then(function(response){
         if (response.ok) {
             response.json().then(function(data){
                 console.log(data);
+
+                console.log(data.results[0].question);
+                showQuestions(data);
             })
         } else {
             // Change to a modal
             alert("countDown is down");
         }
     });
+};
+
+const showQuestions = function(data) {
+    $(".questions-container").empty().append(data.results[0].question);
+
+    const answers = [];
+    answers.push(data.results[0].correct_answer);
+    for (let i = 0; i < data.results[0].incorrect_answers.length; i++) {
+        answers.push(data.results[0].incorrect_answers[i]);
+    }
+    console.log(answers);
+    $(".answers-container").empty();
+    for (let i = 0; i < answers.length; i++) {
+        const button = $("<button/>").text(answers[i]).click(function() {
+            console.log("It's working");
+        })
+        $(".answers-container").append(button);
+    }
 }
 
-questions();
+getQuestions();
 
