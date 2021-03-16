29e6d0be92714caab17fd95f8a28db33062b2aa0
Graham Hall <ghall89@me.com>
2021-03-16 16:44:49 -0400

    sort high scores

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 
 let score = 0;
 let scoreList = [];
+let highScoreList = [];
 let time;
 let timer;
 
@@ -129,8 +130,37 @@ const setHighScore = (initials) => {
 
 const displayHighScore = function() {
   $("#high-scores").empty();
+
+    while (highScoreList.length < 5) {
+        let highestScore = {
+            initials: "",
+            score: 0
+        };
+
+        let index;
+
+        for (let i = 0; i < scoreList.length; i++) {
+            let currentScore = scoreList[i];
+            console.log(currentScore);
+            
+            if (currentScore.score > highestScore.score) {
+                highestScore = currentScore;
+                index = i;
+            }
+            console.log(highestScore);
+    
+        }
+
+        highScoreList.push(highestScore);
+        scoreList.splice(scoreList[index], 1);
+        console.log(scoreList);
+        console.log(highScoreList);
+        
+    }
+
+
   for (let i = 0; i < scoreList.length && i < 5; i++) {
-    $("#high-scores").append(`<li>${scoreList[i].initials}, score: ${scoreList[i].score}</li>`);
+    $("#high-scores").append(`<li>${highScoreList[i].initials}, score: ${highScoreList[i].score}</li>`);
   }
 }
 
