c4db2faf6861c808582fe1c40ccd69e99d7b3a0f
Clayton Goff <cltngoff@gmail.com>
2021-03-17 17:24:33 -0700

    modal modifications

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,9 +5,9 @@ let highScoreList = [];
 let time;
 let timer;
 
-const decrement = function() {
+const decrement = function () {
     if (time > 0) {
-        time = time -1;
+        time = time - 1;
         $(".countdown-counter").empty().append(time);
     } else if (time <= 0) {
         endQuiz();
@@ -18,13 +18,13 @@ const decrement = function() {
 // get the difficulty from the select input
 
 
-const getQuestions = function() {
+const getQuestions = function () {
     var difficulty = $("#difficulty").val()
     var questionCategory = $("#category").val()
     const apiVariable = `https://opentdb.com/api.php?amount=15&category=${questionCategory}&difficulty=${difficulty}`;
-    fetch(apiVariable).then(function(response){
+    fetch(apiVariable).then(function (response) {
         if (response.ok) {
-            response.json().then(function(data){
+            response.json().then(function (data) {
                 console.log(data);
                 let questionNumber = 0;
                 time = 150;
@@ -33,19 +33,18 @@ const getQuestions = function() {
                 showQuestions(data, questionNumber);
             })
         } else {
-            // Change to a modal
-            alert("countDown is down");
+            $("#alert-modal").show();
         }
     });
 };
 
-const showQuestions = function(data, questionNumber) {
+const showQuestions = function (data, questionNumber) {
     if (questionNumber == 5) {
         endQuiz();
         return;
     }
-    $(".selections > form > button").prop( "disabled", true );
-    $(".selections > form > select").prop( "disabled", true );
+    $(".selections > form > button").prop("disabled", true);
+    $(".selections > form > select").prop("disabled", true);
     $(".questions-container").empty().append(data.results[questionNumber].question);
 
     const answers = [];
@@ -56,23 +55,23 @@ const showQuestions = function(data, questionNumber) {
     var item = []
     // randomize the answers for buttons
     while (answers.length > 0) {
-    num = Math.floor(Math.random() * answers.length)
-    item.push(answers[num]);
-    answers.splice(num, 1);
+        num = Math.floor(Math.random() * answers.length)
+        item.push(answers[num]);
+        answers.splice(num, 1);
     }
     // create buttons
     $(".answers-container").empty();
     for (let i = 0; i < item.length; i++) {
-        const button = $("<button>").html(item[i]).click(function() {
+        const button = $("<button>").html(item[i]).click(function () {
             if (this.innerText === data.results[questionNumber].correct_answer) {
                 $(".answers-container").empty().append("Correct!");
                 score = score + 25;
             } else {
                 $(".answers-container").empty().append("Wrong!");
             }
-            setTimeout(function(){
-              questionNumber = questionNumber + 1;
-              showQuestions(data, questionNumber);
+            setTimeout(function () {
+                questionNumber = questionNumber + 1;
+                showQuestions(data, questionNumber);
             }, 2000)
 
         })
@@ -80,78 +79,77 @@ const showQuestions = function(data, questionNumber) {
     }
 
 }
-
-endQuiz = function() {
+// call the times up or congralutations/high score modal
+endQuiz = function () {
     clearInterval(timer);
 
-    $(".selections > form > button").prop( "disabled", false );
-    $(".selections > form > select").prop( "disabled", false );
+    $(".selections > form > button").prop("disabled", false);
+    $(".selections > form > select").prop("disabled", false);
     $(".questions-container").empty();
     $(".answers-container").empty();
 
     score = score + time;
-    
+
     let gifSearch = "";
-    
+
     if (time == 0) {
-      $(".countdown-counter").empty().append("Time's Up!");
-      gifSearch = "timesup"
+        $(".countdown-counter").empty().append("Time's Up!");
+        gifSearch = "timesup"
     } else {
-      gifSearch = "congratulations ";
-      score = score + time;
-      var modal = $(".modal");
-      modal.show();
-      console.log(`Score: ${score}`);
-      
-      
+        gifSearch = "congratulations ";
+        score = score + time;
+        var modal = $("#score-modal");
+        modal.show();
+        $("#new-score").text(`Score: ${score}`)
+        console.log(`Score: ${score}`);
     }
 
     fetch(`https://api.giphy.com/v1/gifs/search?q=${gifSearch}&api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN`)
-        .then(function(response){
+        .then(function (response) {
             return response.json();
         })
-  
-    .then(function(response) {
-        let num2 = Math.floor(Math.random() * 50);
-        var gifImg = document.createElement('img');
-        gifImg.setAttribute('src', response.data[num2].images.fixed_height.url);
-        $('.questions-container').append(gifImg);
-  });
+
+        .then(function (response) {
+            let num2 = Math.floor(Math.random() * 50);
+            var gifImg = document.createElement('img');
+            gifImg.setAttribute('src', response.data[num2].images.fixed_height.url);
+            $('.questions-container').append(gifImg);
+        });
 };
 
 const setHighScore = (initials) => {
-  let highScore = {
-    initials: initials,
-    score: score
-  };
-  console.log(highScore);
-  scoreList.push(highScore);
-  writeToStorage();
-  displayHighScore();
+    let highScore = {
+        initials: initials,
+        score: score
+    };
+    console.log(highScore);
+    scoreList.push(highScore);
+    writeToStorage();
+    displayHighScore();
 }
 
-const displayHighScore = function() {
-  $("#high-scores").empty();
+const displayHighScore = function () {
+    $("#high-scores").empty();
 
     console.log(scoreList);
-    scoreList.sort((a,b) => {return b.score - a.score;});
+    scoreList.sort((a, b) => { return b.score - a.score; });
     console.log(scoreList);
 
-  for (let i = 0; i < scoreList.length && i < 5; i++) {
-    $("#high-scores").append(`<li>${scoreList[i].initials}, score: ${scoreList[i].score}</li>`);
-  }
+    for (let i = 0; i < scoreList.length && i < 5; i++) {
+        $("#high-scores").append(`<li>${scoreList[i].initials}, score: ${scoreList[i].score}</li>`);
+    }
 }
 
-$("#start").on("click", function(){
-  event.preventDefault();
-  $(".countdown-counter").empty();
-  getQuestions();
+$("#start").on("click", function () {
+    event.preventDefault();
+    $(".countdown-counter").empty();
+    getQuestions();
 });
 
-$("#submit-btn").on("click", function(){
+$("#submit-btn").on("click", function () {
     event.preventDefault();
     var initialsStart = $(".initial").val();
-    initials = initialsStart.substring(0,3);
+    initials = initialsStart.substring(0, 3);
     console.log(initials);
     console.log(this);
     $(".modal").hide();
@@ -159,7 +157,10 @@ $("#submit-btn").on("click", function(){
     setHighScore(initials);
 });
 
-$(".initial").keyup(function() {
+$("#alert-btn").on("click", function () {
+    $("#alert-modal").hide();
+});
+$(".initial").keyup(function () {
 
     let input = $(".initial").val();
 
@@ -174,18 +175,18 @@ $(".initial").keyup(function() {
 // getQuestions();
 // Write to localStorage ********************************
 // Pass a full object array nameObjArry = [{initial: xyz, score: 123}, {initial: abc, score: 345}, .....]
-var writeToStorage = function() {
+var writeToStorage = function () {
     localStorage.setItem("scoreList", JSON.stringify(scoreList));
 }
 
 // Return what's stored in localStorage *******************************
-var readFromStorage = function() {
+var readFromStorage = function () {
     scoreList = JSON.parse(localStorage.getItem("scoreList"));
     console.log(scoreList);
     if (scoreList) {
-      displayHighScore();
+        displayHighScore();
     } else {
-      scoreList = [];
+        scoreList = [];
     }
 }
 
